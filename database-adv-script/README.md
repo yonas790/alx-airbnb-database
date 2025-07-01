03134e325f244c559e44659f53923e4b2c4fc813
yonas790 <yoni2752@gmail.com>
2025-07-01 23:40:59 +0300

    Complex Queries with Joins

---
+++ database-adv-script/README.md
@@ -0,0 +1,1 @@
+# 0. Complex Queries with Joins
---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,54 @@
+-- INNER JOIN: Retrieve bookings with user details
+SELECT 
+    b.booking_id, 
+    b.start_date, 
+    b.end_date, 
+    b.total_price, 
+    u.first_name, 
+    u.last_name, 
+    u.email
+FROM 
+    bookings b
+INNER JOIN 
+    users u ON b.user_id = u.user_id;
+
+-- LEFT JOIN: Retrieve all properties with their reviews (including properties without reviews)
+SELECT 
+    p.property_id, 
+    p.name, 
+    p.description, 
+    r.review_id, 
+    r.rating, 
+    r.comment
+FROM 
+    properties p
+LEFT JOIN 
+
+-- FULL OUTER JOIN: Retrieve all users and bookings 
+-- Note: Not all databases support FULL OUTER JOIN directly
+-- This is an emulation using LEFT and RIGHT joins
+SELECT 
+    u.user_id, 
+    u.first_name, 
+    u.last_name, 
+    b.booking_id, 
+    b.start_date, 
+    b.end_date
+FROM 
+    users u
+LEFT JOIN 
+    bookings b ON u.user_id = b.user_id
+UNION
+SELECT 
+    u.user_id, 
+    u.first_name, 
+    u.last_name, 
+    b.booking_id, 
+    b.start_date, 
+    b.end_date
+FROM 
+    users u
+RIGHT JOIN 
+    bookings b ON u.user_id = b.user_id
+WHERE 
+    u.user_id IS NULL;
